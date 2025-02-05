82b45139084e78ff411ff4205f8f6e2d283586f6
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2025-02-05 15:55:34 -0700

    increased accessibility score to 100

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,5 @@
 const selectElement = document.getElementById('theme-select');
 const logoImage = document.getElementById('logo-image');
-
 selectElement.addEventListener('change', changeTheme);
 
 // Change the theme of the page based on the value of the select element
@@ -11,10 +10,12 @@ function changeTheme() {
     if (theme === 'light') {
         logoImage.src = 'byui-logo_blue.webp';
         document.body.classList.remove('dark');
+        selectElement.classList.remove('dark');
     } 
     // Add dark class and set logo image to dark theme logo
     else if (theme === 'dark') {
         logoImage.src = 'byui-logo_white.png';
         document.body.classList.add('dark');
+        selectElement.classList.add('dark');
     }
 }
