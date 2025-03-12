2b33207625074307319d67ef7746c0034f25fa46
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2025-03-11 18:24:32 -0600

    complete recipe part 2

--- recipes/main.js
+++ recipes/main.js
@@ -1,22 +1,17 @@
 // main.js
 import recipes from "./recipes.mjs";
 
-// Hide / Show the recipe description
-const description = document.querySelector('.recipe-description');
+// Hide / Show the recipe descriptions
 function handleResize() {
-    description?.classList.toggle('hide', window.innerWidth < 600);
+    const descriptions = document.querySelectorAll(".recipe-description");
+    descriptions.forEach(description => {
+        description.classList.toggle('hide', window.innerWidth < 600);
+    });
 }
 window.addEventListener("resize", handleResize);
 window.addEventListener("load", handleResize);
 
-function random(number) {
-    return Math.floor(Math.random() * number);
-}
-
-function getRandomListEntry(list) {
-    return list[random(list.length)];
-}
-
+// Template Functions
 function recipeTemplate(recipe) {
     return `<section class="recipe-card">
                 <img class="recipe-image" src="${recipe.image}" alt="${recipe.description}">
@@ -35,7 +30,6 @@ function tagsTemplate(tags) {
     return tags.map(tag => `\t<li>${tag}</li>`).join('\n');
 }
 
-
 function ratingTemplate(rating) {
     let html = `\n<span class="rating" role="img" aria-label="Rating: ${rating} out of 5 stars">\n`;
 
@@ -52,17 +46,48 @@ function ratingTemplate(rating) {
 }
 
 function renderRecipes(recipeList) {
-
     const mainElement = document.querySelector('main');
     const recipesHTML = recipeList.map(recipe => recipeTemplate(recipe)).join('\n');
     mainElement.innerHTML = recipesHTML;
 }
 
+// Helper functions for displaying a random recipe for page load
+function random(number) {
+    return Math.floor(Math.random() * number);
+}
+
+function getRandomListEntry(list) {
+    return list[random(list.length)];
+}
+
 function init() {
     const recipe = getRandomListEntry(recipes);
     renderRecipes([recipe]);
+}
+
+// Search Button Logic
+const searchButton = document.querySelector('#search-bar button');
+const searchInputElement = document.querySelector('#search-input');
+
+searchButton.addEventListener("click", searchHandler)
+
+function searchHandler(event) {
+    event.preventDefault();
+    if (searchInputElement.value != null) {
+        renderRecipes(filterRecipes(searchInputElement.value.toLowerCase()));
+    }
     
 }
 
-init();
+function filterRecipes(query) {
+    const filteredRecipes = recipes.filter(recipe =>
+        recipe.name.toLowerCase().includes(query) ||
+        recipe.description.toLowerCase().includes(query) ||
+        recipe.tags.find(tag => tag.toLowerCase().includes(query)) ||
+        recipe.recipeIngredient.find(ingredient => ingredient.toLowerCase().includes(query))
+    );
 
+    return filteredRecipes.sort((a, b) => a.name.localeCompare(b.name));
+}
+
+init();
