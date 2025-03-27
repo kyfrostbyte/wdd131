396688222f5a3e66625120866636cb56cf3bb808
kyfrostbyte <94023660+kyfrostbyte@users.noreply.github.com>
2025-03-26 20:36:01 -0600

    index layout mostly done

--- uptown-management/scripts/main.js
+++ uptown-management/scripts/main.js
@@ -1,7 +1,8 @@
-// Sidebar collapse logic
 const toggleBtn = document.querySelector('.toggle-btn');
 const sidebar = document.querySelector('.sidebar');
+const sidebarSpacer = document.querySelector('#sidebar-spacer');
 
 toggleBtn.addEventListener('click', () => {
   sidebar.classList.toggle('collapsed');
+  sidebarSpacer.classList.toggle('expanded');
 });
